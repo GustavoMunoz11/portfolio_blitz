64e8685ee6349fd94c8bfa5834d233e23440c3bd
GustavoMunoz11 <guss03112002@gmail.com>
2024-06-06 17:09:15 -0500

    links in the header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,34 +1,36 @@
 <script setup>
-
+import { useRouter } from 'vue-router';
+const router = useRouter();
 </script>
 
 
 <template>
     <header>
         <h1>portafolio</h1>
+
         <div class="buttons">
-            <p>about</p>
-        <p>contact</p>
-        <p>works</p>
+            <p @click="router.push({ name: 'about' })">about</p>
+            <p @click="router.push({ name: 'contact' })">contact</p>
+            <p @click="router.push({ name: 'work' })">work</p>
         </div>
-      
+
 
     </header>
 
 </template>
 
 <style scoped>
-
 header {
-    background-color:blueviolet;
+    background-color: blueviolet;
     display: flex;
     justify-content: space-between;
     padding: 20px;
 }
+
 h1 {
     color: whitesmoke;
     font-size: 2rem;
-    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 
 .buttons {
