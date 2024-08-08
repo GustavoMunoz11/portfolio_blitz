34d5a73940b9865853df3392cf04e9bd837de6cd
Gustavo <guss03112002@gmail.com>
2024-08-08 16:43:36 -0500

    About and CardAbout

--- src/components/works/About.vue
+++ src/components/works/About.vue
@@ -1,15 +1,11 @@
-<script setup></script>
+<script setup>
+import CardAbout from './CardAbout.vue';
+</script>
 
 <template>
 
     <section>
-<h1>Sobre mi</h1>
-        <div class="about">
-        <div id="foto"></div>
-        <p>Nací en barrancabermeja santander, desde pequeño siempre <br>me han apasionado las historias
-            y el poder escribir las mias.</br>
-        </p>
-    </div>
+        <CardAbout />
     </section>
 
 </template>
@@ -18,36 +14,4 @@
 section{
     background-color: var(--color-third)
 }
-
-h1{
-    font-family: "Roboto", bold;
-  color: var(--color-primary);
-  text-align: center;
-  top: 40px;
-}
-
-.about{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding-top: 100px;
-    
-}
-
-#foto{
-    border-radius: 100%;
-    width: 300px;
-    height: 300px;
-    background-color: gray;
-}
-
-p{
-    padding-top: 50px;
-    padding-bottom: 100px;
-    font-family: "Roboto";
-    font-size: 20px;
-    color: var(--color-fourth);
-}
-
 </style>
---
+++ src/components/works/CardAbout.vue
@@ -0,0 +1,48 @@
+<script setup></script>
+
+<template>
+        <div class="about">
+<h1>Sobre mi</h1>
+        <div id="foto"></div>
+        <p>Nací en barrancabermeja santander, desde pequeño siempre <br>me han apasionado las historias
+            y el poder escribir las mias.</br>
+        </p>
+    </div>
+
+</template>
+
+<style scoped>
+
+
+h1{
+    font-family: "Roboto", bold;
+  color: var(--color-primary);
+  text-align: center;
+  top: 40px;
+}
+
+.about{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 100px;
+    
+}
+
+#foto{
+    border-radius: 100%;
+    width: 300px;
+    height: 300px;
+    background-color: gray;
+}
+
+p{
+    padding-top: 50px;
+    padding-bottom: 100px;
+    font-family: "Roboto";
+    font-size: 20px;
+    color: var(--color-fourth);
+}
+
+</style>
